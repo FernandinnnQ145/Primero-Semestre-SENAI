2008ca25aaf6e97c7a9c9932f7d91a546fa859fb
Fernando Quatrochi <fernandinnn145@gmail.com>
2023-03-29 16:58:03 -0300

    Mais alguns exercicios de c# concluidos

---
+++ beckend-1/exercicio-003/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine($"Digite seu nome:");
+string nome = Console.ReadLine();
+
+float n1,n2,n3,n4,n5, media;
+
+Console.WriteLine($"Digite a primeira nota:");
+n1 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Digite a segunda nota:");
+n2 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Digite a terceira nota:");
+n3 = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Digite a quarta nota:");
+n4 = float.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine($"Digite a quinta nota:");
+n5 = float.Parse(Console.ReadLine());
+
+
+media = (n1+n2+n3+n4+n5)/5;
+
+Console.WriteLine($"A media do {nome} é {media}");
+
+if (media>=6){
+    Console.WriteLine($"Aprovado");
+    
+}
+else{
+    Console.WriteLine($"Reprovado");
+    
+}
+
+
+
+
---
+++ beckend-1/exercicio-004/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine($"Digite quantos kg de arroz voce quer:");
+
+float kg = float.Parse(Console.ReadLine());
+float valor;
+
+// Valor do kg pre-definido
+
+valor = kg * 6;
+
+Console.WriteLine($"O valor a ser pago sera R$ {valor}");
+
