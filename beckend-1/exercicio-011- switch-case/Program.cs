57fd7b540f6c2e1aaaeab2231d3fdc201adb2bbc
Fernando Quatrochi <fernandinnn145@gmail.com>
2023-04-04 15:17:05 -0300

    Exercicio de switch case concluido

--- beckend-1/exercicio-011- switch-case/Program.cs
+++ beckend-1/exercicio-011- switch-case/Program.cs
@@ -11,100 +11,105 @@ string gelo;
 switch (escolha)
 {
     case 1:
-    Console.WriteLine($"Deseja colocar gelo na sua coca-cola?");
+        Console.WriteLine($"Deseja colocar gelo na sua coca-cola?");
 
 
-    gelo = Console.ReadLine().ToUpper();
-    if (gelo == "sim")
-    {
-        Console.WriteLine($"Sua coca-cola sera com adicional de gelo");
-        
-    }
-    else{
-        Console.WriteLine($"Sua coca-cola não tera adicional de gelo");
-        
-    }
+        gelo = Console.ReadLine().ToLower();
+        if (gelo == "sim")
+        {
+            Console.WriteLine($"Sua coca-cola sera com adicional de gelo");
 
-    break;
+        }
+        else
+        {
+            Console.WriteLine($"Sua coca-cola não tera adicional de gelo");
+
+        }
+
+        break;
 
     case 2:
-    Console.WriteLine($"Deseja colocar gelo na sua cerveja?");
+        Console.WriteLine($"Deseja colocar gelo na sua cerveja?");
 
- gelo = Console.ReadLine().ToUpper();
-    if (gelo == "sim")
-    {
-        Console.WriteLine($"Sua cerveja sera com adicional de gelo");
-        
-    }
-    else{
-        Console.WriteLine($"Sua cerveja não tera adicional de gelo");
-        
-    }
+        gelo = Console.ReadLine().ToLower();
+        if (gelo == "sim")
+        {
+            Console.WriteLine($"Sua cerveja sera com adicional de gelo");
 
-    break;
+        }
+        else
+        {
+            Console.WriteLine($"Sua cerveja não tera adicional de gelo");
+
+        }
+
+        break;
 
 
     case 3:
-    Console.WriteLine($"Deseja colocar gelo na sua caipirinha?");
+        Console.WriteLine($"Deseja colocar gelo na sua caipirinha?");
 
 
- gelo = Console.ReadLine().ToUpper();
-    if (gelo == "sim")
-    {
-        Console.WriteLine($"Sua caipirinha sera com adicional de gelo");
-        
-    }
-    else{
-        Console.WriteLine($"Sua caipirinha não tera adicional de gelo");
-        
-    }
+        gelo = Console.ReadLine().ToLower();
+        if (gelo == "sim")
+        {
+            Console.WriteLine($"Sua caipirinha sera com adicional de gelo");
+
+        }
+        else
+        {
+            Console.WriteLine($"Sua caipirinha não tera adicional de gelo");
+
+        }
 
 
 
-    break;
+        break;
 
 
 
     case 4:
-    Console.WriteLine($"Deseja colocar gelo no seu suco?");
+        Console.WriteLine($"Deseja colocar gelo no seu suco?");
 
 
-     gelo = Console.ReadLine().ToUpper();
-    if (gelo == "sim")
-    {
-        Console.WriteLine($"Seu suco de laranja sera com adicional de gelo");
-        
-    }
-    else{
-        Console.WriteLine($"Seu suco de laranja não tera adicional de gelo");
-        
-    }
-    break;
+        gelo = Console.ReadLine().ToLower();
+        if (gelo == "sim")
+        {
+            Console.WriteLine($"Seu suco de laranja sera com adicional de gelo");
+
+        }
+        else
+        {
+            Console.WriteLine($"Seu suco de laranja não tera adicional de gelo");
+
+        }
+        break;
 
 
 
     case 5:
-    Console.WriteLine($"Deseja colocar gelo no seu guarana?");
+        Console.WriteLine($"Deseja colocar gelo no seu guarana?");
 
-     gelo = Console.ReadLine().ToUpper();
-    if (gelo == "sim")
-    {
-        Console.WriteLine($"Seu guarana sera com adicional de gelo");
-        
-    }
-    else{
-        Console.WriteLine($"Seu guarana não tera adicional de gelo");
-        
-    }
+        gelo = Console.ReadLine().ToLower();
+        if (gelo == "sim")
+        {
+            Console.WriteLine($"Seu guarana sera com adicional de gelo");
 
-    break;
+        }
+        else
+        {
+            Console.WriteLine($"Seu guarana não tera adicional de gelo");
+
+        }
+
+        break;
 
 
     default:
-    Console.WriteLine($"não temos essa opção");
-    break;
+        Console.WriteLine($"não temos essa opção");
+        break;
 
-    
+
 }
 
 
