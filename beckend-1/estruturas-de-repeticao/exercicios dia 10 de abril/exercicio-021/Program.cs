5f20227c3f9cee112032668277b9b753f0433e65
Fernando Quatrochi <fernandinnn145@gmail.com>
2023-04-10 15:04:29 -0300

    Exercicios concluidos

---
+++ beckend-1/estruturas-de-repeticao/exercicios dia 10 de abril/exercicio-021/Program.cs
@@ -0,0 +1,44 @@
+﻿int masculino = 0;
+int feminino = 0;
+int iMasculino = 0;
+int iFeminino = 0;
+
+for (int quantidade = 0; quantidade < 10; quantidade++)
+{
+
+
+
+
+    Console.WriteLine($"Escreva a sua idade");
+    int idade = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Escreva o seu peso");
+    int peso = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Escreva seu sexo, m/f");
+    char sexo = char.Parse(Console.ReadLine());
+    if (sexo != 'f' && sexo != 'm')
+    {
+        Console.WriteLine($"Resposta invalida, tente outra");
+        sexo = char.Parse(Console.ReadLine());
+
+    }
+    else if (sexo == 'm'){
+        masculino++;
+        iMasculino += idade;
+    }
+    else{
+        feminino++;
+        iFeminino += idade;
+    }
+}
+
+Console.WriteLine($"Total de homens é {masculino}");
+Console.WriteLine($"Total de feminino é {feminino}");
+Console.WriteLine($"Total da media de idades dos homens é {iMasculino / masculino}");
+Console.WriteLine($"Total da media de idades das mulheres é {iFeminino / feminino}");
+
+
+
+
+
---
+++ beckend-1/estruturas-de-repeticao/exercicios dia 10 de abril/exercicios-023-desafio/Program.cs
@@ -0,0 +1,17 @@
+﻿int pValor, sValor, resultado;
+pValor=1;
+sValor=1;
+Console.WriteLine($"{pValor}");
+Console.WriteLine($"{sValor}");
+
+
+
+do{
+
+    resultado = pValor + sValor;
+    Console.WriteLine($"{resultado}");
+    sValor=pValor;
+    pValor=resultado;
+    
+    
+}while(resultado<500);
