337386cce4d6bc699163279eb5e12f2ff866eaf5
Fernando Quatrochi <fernandinnn145@gmail.com>
2023-04-19 14:42:33 -0300

    Projeto passagem concluido

---
+++ beckend-1/exercicio-039-projetoPassagem/Program.cs
@@ -0,0 +1,250 @@
+﻿// Nesta aula vamos aplicar o seguinte projeto para gerenciamento de passagens aéreas pelo console:
+
+// Criar uma aplicação para uma agência de turismo, no qual deveremos registrar passagens aéreas com os seguintes dados: Nome do passageiro, Origem, Destino e Data do Voo de 5 passageiros.
+
+// Antes de entrar no sistema faça um esquema do qual o usuário só possa acessar o menu se a senha for igual à 123456.
+// O sistema deve exibir um menu com as seguintes opções:
+
+// 1- Cadastrar passagem
+// 2- Listar Passagens
+// 0- Sair
+// Observação :  Criar ao menos uma função (Efetuar Login).
+
+// Ao cadastrar uma passagem ao final o sistema deverá perguntar se gostaria de cadastrar uma nova passagem caso contrário voltar ao menu anterior(S/N).
+
+
+
+
+{
+    login();
+}
+
+static void login()
+{
+
+
+    Console.WriteLine($"Digite a senha:");
+
+    int senha = int.Parse(Console.ReadLine()!);
+    while (senha != 123456)
+    {
+        Console.WriteLine($"Senha invalida, tente outra:");
+        senha = int.Parse(Console.ReadLine()!);
+
+    }
+
+}
+
+int escolha;
+
+int opcao;
+
+string[] nome = new string[5];
+string[] origem = new string[5];
+string[] destino = new string[5];
+string[] data = new string[5];
+
+
+
+
+
+do
+{
+    Console.Clear();
+    //criar menu de opções
+    Console.WriteLine($"Menu de opções");
+
+    Console.WriteLine(@$"
+Selecione um das opções
+[1] - Cadastrar
+[2] - Listar
+[0] - Sair
+");
+
+    opcao = int.Parse(Console.ReadLine());
+
+    string resposta;
+
+    switch (opcao)
+    {
+        case 1:
+            do
+            {
+                for (var i = 0; i < 2; i++)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Informe o nome: ");
+                    nome[i] = Console.ReadLine();
+
+                    Console.WriteLine($"Informe a origem: ");
+                    origem[i] = Console.ReadLine();
+
+                    Console.WriteLine($"Informe o destino: ");
+                    destino[i] = Console.ReadLine();
+
+                    Console.WriteLine($"Informe a data: ");
+                    data[i] = Console.ReadLine();
+                }
+
+                Console.WriteLine($"Quer cadastrar mais passagens: s/n");
+                resposta = Console.ReadLine().ToLower();
+
+            } while (resposta == "s");
+            break;
+        case 2:
+            for (var i = 0; i < 2; i++)
+            {
+                Console.WriteLine(@$"
+                *******************
+                Passagens - Bilhete
+
+                Nome: {nome[i]}
+                Origem: {origem[i]}
+                Destino: {destino[i]}
+                Data: {data[i]}
+                ");
+            }
+            Console.WriteLine($"Aperte enter para voltar");
+            Console.ReadLine();
+            
+            
+            break;
+        case 0:
+             Console.Clear();
+            System.Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine($"Opção inválida!");
+            break;
+    }
+} while (opcao != 0);
+
+
+
+// // // // Criar uma aplicação para uma agência de turismo, no qual deveremos registrar passagens aéreas com os seguintes dados: Nome do passageiro, Origem, Destino e Data do Voo de 5 passageiros.
+
+// // // Antes de entrar no sistema faça um esquema do qual o usuário só possa acessar o menu se a senha for igual à 123456.
+
+// // // O sistema deve exibir um menu com as seguintes opções:
+
+// // // 1- Cadastrar passagem
+// // // 2- Listar Passagens
+// // // 0- Sair
+
+
+// // // Ao cadastrar uma passagem ao final, o sistema deverá perguntar se gostaria de cadastrar uma nova passagem caso contrário voltar ao menu anterior(S/N).
+
+
+// // //Algoritmo
+// // //lista para armazenar nomes,origens,destinos e datas ok 
+// // //validação de senha ok
+// // entrada da senha pelo usuário ok
+// // //menu de opções
+// // //opção de cadastrar novas passagens
+
+// //criando as funções
+
+
+// //estrutura de uma função
+
+// //tipo tipoDado nomeFuncao(parâmetros ou argumentos)
+// {
+//     //Corpo da função
+// }
+
+// //função para verificar senha
+// static bool Login(string senha)
+// {
+//     if (senha == "123456")
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Senha incorreta!");
+//         return false;
+//     }
+// }
+
+
+// string[] nomes = new string[2];
+// string[] origens = new string[2];
+// string[] destinos = new string[2];
+// string[] datas = new string[2];
+
+// bool senhaValida;
+
+// do
+// {
+//     Console.WriteLine($"Informe a sua senha: ");
+//     string senha = Console.ReadLine();
+
+//     senhaValida = Login(senha);
+
+// } while (senhaValida != true);
+
+// string opcao;
+
+// do
+// {
+//     //criar menu de opções
+//     Console.WriteLine($"Menu de opções");
+
+//     Console.WriteLine(@$"
+// Selecione um das opções
+// [1] - Cadastrar
+// [2] - Listar
+// [0] - Sair
+// ");
+
+//     opcao = Console.ReadLine();
+
+//     string resposta;
+
+//     switch (opcao)
+//     {
+//         case "1":
+//             do
+//             {
+//                 for (var i = 0; i < 2; i++)
+//                 {
+//                     Console.WriteLine($"Informe o nome: ");
+//                     nomes[i] = Console.ReadLine();
+
+//                     Console.WriteLine($"Informe a origem: ");
+//                     origens[i] = Console.ReadLine();
+
+//                     Console.WriteLine($"Informe o destino: ");
+//                     destinos[i] = Console.ReadLine();
+
+//                     Console.WriteLine($"Informe a data: ");
+//                     datas[i] = Console.ReadLine();
+//                 }
+
+//                 Console.WriteLine($"Quer cadastrar mais passagens: s/n");
+//                 resposta = Console.ReadLine().ToLower();
+
+//             } while (resposta == "s");
+//             break;
+//         case "2":
+//             for (var i = 0; i < 2; i++)
+//             {
+//                 Console.WriteLine(@$"
+//                 *******************
+//                 Passagens - Bilhete
+
+//                 Nome: {nomes[i]}
+//                 Origem: {origens[i]}
+//                 Destino: {destinos[i]}
+//                 Data: {datas[i]}
+//                 ");
+//             }
+//             break;
+//         case "0":
+//             Console.WriteLine($"Fim do programa");
+//             break;
+//         default:
+//             Console.WriteLine($"Opção inválida!");
+//             break;
+//     }
+// } while (opcao != "0");
---
+++ beckend-1/exercicios-beckend-geral/exercicio-038-desafio/Program.cs
@@ -0,0 +1,388 @@
+﻿// função para voltar
+static void volta()
+{
+    Console.WriteLine($"\n");
+
+    Console.WriteLine($"Aperte enter para voltar");
+    Console.ReadLine();
+    Console.Clear();
+    opcoes();
+}
+
+// função para exercicio concluido
+static void concluido()
+{
+    Console.Clear();
+    Console.WriteLine($"Exercicio concluido, aperte enter para continuar");
+    Console.ReadLine();
+
+    Console.Clear();
+}
+
+// função para exercicio em andamento
+static void andamento()
+{
+    Console.Clear();
+    Console.WriteLine($"Exercicio em andamento, aperte qualquer tecla para voltar");
+    Console.ReadKey();
+
+    Console.Clear();
+    opcoes();
+}
+
+
+// função principal e para as opções
+static void opcoes()
+{
+    Console.WriteLine(@$"Escolha qual exercicio deseja ver:
+1-Exercicio idade para votar
+2-exercicio posto
+3-exercicio desconto
+4-exercicio menor e maior
+5-exercicio tabuada
+6-exercicio busca
+7-exercicio ordem
+8-Apenas um exemplo de exercicio não concluido
+
+0-para sair");
+
+    int escolha = int.Parse(Console.ReadLine());
+
+
+
+
+    switch (escolha)
+    {
+        // exercicio 1
+        case 1:
+            concluido();
+            Console.WriteLine($"Digite o ano em que nasceu:");
+            int ano = int.Parse(Console.ReadLine());
+
+            if (ano >= 2008)
+            {
+                Console.WriteLine($"Voce nao pode votar");
+
+            }
+            else if ((ano >= 2005) && (ano <= 2007))
+            {
+                Console.WriteLine($"Voce tem a opção de votar");
+
+            }
+            else
+            {
+                Console.WriteLine($"Voce é obrigado a votar");
+
+            }
+            volta();
+
+
+            break;
+
+
+
+
+
+
+
+
+
+        // exercicio 2
+        case 2:
+            concluido();
+            static float alcool(float litros)
+            {
+
+                if (litros <= 20)
+                {
+                    float r = (litros * 4.90f) * 0.97f;
+                    return r;
+                }
+                else
+                {
+                    float r = (litros * 4.90f) * 0.95f;
+                    return r;
+                }
+            }
+
+
+            static float gasolina(float litros)
+            {
+                if (litros <= 20)
+                {
+                    float r = (litros * 5.30f) * 0.96f;
+                    return r;
+                }
+                else
+                {
+                    float r = (litros * 5.30f) * 0.94f;
+                    return r;
+                }
+
+            }
+            char escolha1;
+            do
+            {
+                Console.WriteLine(@$"Selecione qual tipo de combustivel voce deseja:
+a-alcool 
+g-gasolina");
+                escolha1 = char.Parse(Console.ReadLine().ToLower());
+
+
+                switch (escolha1)
+                {
+                    case 'a':
+                        Console.WriteLine($"\nQuantos litros voce deseja?");
+                        float litros = float.Parse(Console.ReadLine());
+                        Console.WriteLine($"\nVoce devera pagar R${alcool(litros)}");
+                        break;
+
+
+                    case 'g':
+                        Console.WriteLine($"\nQuantos litros voce deseja?");
+                        litros = float.Parse(Console.ReadLine());
+                        Console.WriteLine($"\nVoce devera pagar R${gasolina(litros)}");
+                        break;
+                    default:
+                        Console.WriteLine($"Resposta invalida, tente outra:");
+                        escolha = char.Parse(Console.ReadLine().ToLower());
+                        break;
+                }
+
+
+
+
+
+
+            } while ((escolha1 != 'a' && escolha != 'g'));
+
+            volta();
+            break;
+
+
+
+
+
+
+        // exercicio 3
+        case 3:
+            concluido();
+            Console.WriteLine($"Digite o nome do produto");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Digite a quantidade do produto");
+            int quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Digite o preço do produto");
+            float preco = float.Parse(Console.ReadLine());
+
+            float valor = quantidade * preco;
+            float desconto;
+            if (quantidade <= 5)
+            {
+                desconto = valor - ((2 / 100f) * valor);
+            }
+
+
+            else if (quantidade > 5 && quantidade <= 10)
+            {
+                desconto = valor - ((3 / 100f) * valor);
+                Console.WriteLine($"Voce pagara R${desconto} pelo {nome}");
+            }
+            else
+            {
+                desconto = valor - ((5 / 100f) * valor);
+                Console.WriteLine($"Voce pagara R${desconto} pelo {nome}");
+            }
+
+
+            volta();
+            break;
+
+
+
+
+
+
+
+
+
+        // exercicio 4
+        case 4:
+            concluido();
+            int[] numerosExercicio4 = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}º valor");
+                numerosExercicio4[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine(@$"O seu maior valor é o {numerosExercicio4.Max()}
+            O seu menor valor é o {numerosExercicio4.Min()}");
+
+            volta();
+            break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // exercicio 5
+        case 5:
+            concluido();
+            int resultado;
+            Console.WriteLine($"Aqui esta a tabuada de 1 a 10");
+
+            for (int pMultiplicador = 0; pMultiplicador <= 10; pMultiplicador++)
+            {
+                for (int sMultiplicador = 0; sMultiplicador <= 10; sMultiplicador++)
+                {
+
+
+                    resultado = sMultiplicador * pMultiplicador;
+                    Console.WriteLine($"{pMultiplicador}x{sMultiplicador} = {resultado}");
+                }
+            }
+
+
+            volta();
+            break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // exercicio 6
+        case 6:
+            concluido();
+            string[] nomes = new string[10];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}º nome:");
+                nomes[i] = Console.ReadLine().ToLower();
+
+
+            }
+
+            Console.WriteLine($"Digite um nome para conferir se ele esta na lista:");
+            string bNome = Console.ReadLine();
+
+            // if (nomes.Contains(bNome))
+            // {
+            //     Console.WriteLine($"Achei :)");
+
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"Nao achei :(");
+
+            // }
+            string resultadoBusca = nomes.Contains(bNome)? "\nAchei :)":"\nNão achei :(";
+            Console.WriteLine($"{resultadoBusca}");
+            
+            volta();
+
+            break;
+
+
+
+
+        // exercicio 7
+        case 7:
+            concluido();
+            int[] numeros = new int[15];
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"Entre com o {i + 1}º numero");
+
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine($"Agora a ordem inversa:");
+
+
+            for (int i = 14; i > 0; i--)
+            {
+                Console.WriteLine($"{numeros[i]}");
+
+            }
+
+            volta();
+            break;
+
+
+
+
+
+
+
+
+
+
+        // exemplo de exercicio em andamento
+        case 8:
+            andamento();
+            break;
+
+
+
+
+        // para encerrar o programa
+        case 0:
+            Console.Clear();
+            System.Environment.Exit(0);
+            break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        default:
+
+            Console.Clear();
+            opcoes();
+            break;
+
+
+    }
+}
+{
+    opcoes();
+}
+
+
+
+
