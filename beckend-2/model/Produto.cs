4f148276e9d5ff405a165b5776d8e07cc61de296
Fernando Quatrochi <fernandinnn145@gmail.com>
2023-05-24 14:43:58 -0300

    Pasta atualizada

---
+++ beckend-2/model/Produto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace beckend_2.model
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string? Nome { get; set; }
+        public float Preco { get; set; }
+
+        string nome = "Fernando";
+        
+        
+        
+    }
+}
